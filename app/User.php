bea65ac69fc86bc5b44991888024372947856752
Milan Ljepojevic <ljepojevicm@yahoo.com>
2018-03-18 17:28:19 +0100

    Added relationships and Listings

--- app/User.php
+++ app/User.php
@@ -26,4 +26,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // One to many 
+
+    public function listings(){
+        return $this->hasMany('App\Listing');
+    }
 }
